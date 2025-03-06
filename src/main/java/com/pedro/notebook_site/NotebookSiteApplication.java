efae72721890b6dbbbedef27599dc2c8d63f1bb0
Pedro Eccel Zimmermann <pedroez@weg.net>
2025-03-06 14:00:42 -0300

    Initialize Spring Boot project with Maven setup and basic application structure

---
+++ src/main/java/com/pedro/notebook_site/NotebookSiteApplication.java
@@ -0,0 +1,13 @@
+package com.pedro.notebook_site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotebookSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotebookSiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pedro/notebook_site/NotebookSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedro.notebook_site;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotebookSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
